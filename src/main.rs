80e265e37bf8eddeb348f8c2487e998200e1c1b9
alfonsocv12 <alfonsocvu@gmail.com>
2020-08-28 17:28:31 -0600

    Start adding templates

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,25 @@
 
 #[macro_use] extern crate rocket;
 
+extern crate rocket_contrib;
+extern crate tera;
+
+// Bring both Template and Context into scope
+use rocket_contrib::templates::Template;
+use tera::Context;
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, worlds!"
+fn index() -> Template {
+    let mut context = Context::new();
+    // let context = context();
+
+    context.insert("my_message", &"Heya from template context!");
+    Template::render("layout", &context)
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+        .launch();
 }
