8faf0a74f4128f02586f871e8c4ae44b5329f57d
alfonsocv12 <alfonsocvu@gmail.com>
2020-08-28 20:13:12 -0600

    Trying to make and mvc but failing

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,32 @@
 extern crate rocket_contrib;
 extern crate tera;
 
-// Bring both Template and Context into scope
 use rocket_contrib::templates::Template;
 use std::collections::HashMap;
 
-#[get("/")]
-fn index() -> Template {
-    let mut context = HashMap::new();
-    context.insert("my_message", &"Heya from template context!");
+trait LandingRoutes {
+    fn new() -> Self;
 
-    Template::render("layout", context)
+    #[get("/")]
+    fn index(&self) -> Template {
+        let mut context = HashMap::new();
+        context.insert("my_message", &"Heya from template context!");
+
+        Template::render("layout", context)
+    }
+}
+
+fn array_routes() -> routes{
+    let routes = LandingRoutes::new();
+
+    return routes![
+        routes.index(),
+    ];
 }
 
 fn main() {
     rocket::ignite()
-        .mount("/", routes![index])
+        .mount("/", array_routes())
         .attach(Template::fairing())
         .launch();
 }
